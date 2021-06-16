a413099cddac17e5ef0a439118cc5f31909dad06
Maha Magdy <mahamagdy3@gmail.com>
2021-06-16 20:02:14 +0200

    Add the foreign key of user (who will create the event) in the event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+    # has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :events, inverse_of: 'creator'
+  # has_and_belongs_to_many :events
+
 end
