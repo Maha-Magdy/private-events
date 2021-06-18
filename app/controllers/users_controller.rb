575c9cae122cccfd7598d3cf62bfe4fbff1ad887
Maha Magdy <mahamagdy3@gmail.com>
2021-06-18 20:29:18 +0200

    Allow feature to divide the events into a past events and future ones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-        @past_events = @user.attended_events.where("date < ?", Date.today)
-        @future_events = @user.attended_events.where("date >= ?", Date.today)
+        @past_events = @user.attended_events.past_events
+        @future_events = @user.attended_events.future_events
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,15 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
     has_many :enrollments
     has_many :attendees, :through => :enrollments, source: :user
+
+    # def self.future_events
+    #     where("date >= ?", Date.today)
+    # end
+    scope :future_events, -> { where("date >= ?", Date.today) }
+
+    # def self.past_events
+    #     where("date < ?", Date.today)
+    # end
+    scope :past_events, -> { where("date < ?", Date.today) }
+
 end
